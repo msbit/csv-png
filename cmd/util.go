dac4afe8653f6e10761c971631a02c734dfc5c65
Tom Sullivan <tom@msbit.com.au>
2021-05-30 00:00:28 +1000

    Move `draw_axes` and `draw_data` to `cmd.Image`

--- cmd/util.go
+++ cmd/util.go
@@ -2,6 +2,14 @@ package cmd
 
 type scaler = func(float64) float64
 
+type Options struct {
+	Input  string
+	Output string
+	Width  int
+	Height int
+	Margin int
+}
+
 func Scaler(input_min float64, input_max float64, output_min float64, output_max float64) scaler {
 	scale := (output_max - output_min) / (input_max - input_min)
 	return func(input float64) float64 {
--- main.go
+++ main.go
@@ -6,56 +6,42 @@ import (
 	"image"
 	"image/color"
 	"image/png"
-	"math"
 	"os"
-	"sort"
-
-	_color "github.com/gerow/go-color"
 
 	"github.com/msbit/csv-png/cmd"
 )
 
-type scaler = func(float64) float64
-
-type options_t struct {
-	input  string
-	output string
-	width  int
-	height int
-	margin int
-}
-
 func main() {
-	options := options_t{margin: 54}
+	options := cmd.Options{Margin: 54}
 
-	flag.StringVar(&options.input, "input", "", "Input CSV file")
-	flag.StringVar(&options.output, "output", "", "Output PNG file")
+	flag.StringVar(&options.Input, "input", "", "Input CSV file")
+	flag.StringVar(&options.Output, "output", "", "Output PNG file")
 
-	flag.IntVar(&options.width, "width", 1920, "Output PNG width")
-	flag.IntVar(&options.height, "height", 1080, "Output PNG height")
+	flag.IntVar(&options.Width, "width", 1920, "Output PNG width")
+	flag.IntVar(&options.Height, "height", 1080, "Output PNG height")
 
 	flag.Parse()
 
-	if options.input == "" || options.output == "" {
+	if options.Input == "" || options.Output == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	_, data, err := cmd.ReadInput(options.input)
+	_, data, err := cmd.ReadInput(options.Input)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	img := &cmd.Image{
-		image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{options.width, options.height}}),
+		image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{options.Width, options.Height}}),
 	}
 	img.Fill(color.White)
 
-	draw_axes(img, options)
-	draw_data(img, data, options)
+	img.DrawAxes(options)
+	img.DrawData(data, options)
 
-	output, err := os.Create(options.output)
+	output, err := os.Create(options.Output)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -68,65 +54,4 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func draw_axes(img *cmd.Image, options options_t) {
-	margin := float64(options.margin)
-	width := float64(options.width)
-	height := float64(options.height)
-
-	img.DrawLine(margin, margin, margin, height-margin, color.Black)
-	img.DrawLine(margin, height-margin, width-margin, height-margin, color.Black)
-}
-
-func draw_data(img *cmd.Image, data map[float64][]float64, options options_t) {
-	colours, h_scaler, v_scaler := calculate_attributes(data, options)
-	keys := make([]float64, 0, len(data))
-	for k := range data {
-		keys = append(keys, k)
-	}
-	sort.Float64s(keys)
-	for i := 1; i < len(keys); i++ {
-		x0 := keys[i-1]
-		x1 := keys[i]
-		series0 := data[x0]
-		series1 := data[x1]
-		for j := 0; j < len(series0); j++ {
-			img.DrawLine(h_scaler(x0), v_scaler(series0[j]), h_scaler(x1), v_scaler(series1[j]), colours[j])
-		}
-	}
-}
-
-func calculate_attributes(data map[float64][]float64, options options_t) ([]color.Color, scaler, scaler) {
-	xmin := math.Inf(1)
-	xmax := math.Inf(-1)
-	value_min := math.Inf(1)
-	value_max := math.Inf(-1)
-	series_count := 0
-
-	for x, series := range data {
-		xmin = math.Min(x, xmin)
-		xmax = math.Max(x, xmax)
-		series_count = cmd.Max(series_count, len(series))
-
-		for _, value := range series {
-			value_min = math.Min(value_min, value)
-			value_max = math.Max(value_max, value)
-		}
-	}
-
-	colours := []color.Color{}
-	for i := 0; i < series_count; i++ {
-		hue := float64(i) / float64(series_count)
-		rgb := _color.HSL{hue, 0.5, 0.5}.ToRGB()
-		colours = append(colours, color.RGBA{uint8(rgb.R * 0xff), uint8(rgb.G * 0xff), uint8(rgb.B * 0xff), 0xff})
-	}
-
-	margin := options.margin
-	width := options.width
-	height := options.height
-
-	return colours,
-		cmd.Scaler(xmin, xmax, float64(margin), float64(width-margin)),
-		cmd.Scaler(value_min, value_max, float64(height-margin), float64(margin))
 }
