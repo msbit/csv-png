ab4f46debb763ce61359a40177decbe32cf074f7
Tom <tom@msbit.com.au>
2026-02-19 09:51:12 +1100

    test: better test case structure

--- lib/colour_test.go
+++ lib/colour_test.go
@@ -5,30 +5,35 @@ import (
 	"testing"
 )
 
-var testCases = map[HSL]color.RGBA{
-	/* Black */ HSL{0, 0.0, 0.0}: color.RGBA{0, 0, 0, 255},
-	/* White */ HSL{0, 0.0, 1.0}: color.RGBA{255, 255, 255, 255},
-	/* Red */ HSL{0, 1.0, 0.5}: color.RGBA{255, 0, 0, 255},
-	/* Lime */ HSL{120, 1.0, 0.5}: color.RGBA{0, 255, 0, 255},
-	/* Blue */ HSL{240, 1.0, 0.5}: color.RGBA{0, 0, 255, 255},
-	/* Yellow */ HSL{60, 1.0, 0.5}: color.RGBA{255, 255, 0, 255},
-	/* Cyan */ HSL{180, 1.0, 0.5}: color.RGBA{0, 255, 255, 255},
-	/* Magenta */ HSL{300, 1.0, 0.5}: color.RGBA{255, 0, 255, 255},
-	/* Silver */ HSL{0, 0.0, 0.75}: color.RGBA{191, 191, 191, 255},
-	/* Gray */ HSL{0, 0.0, 0.5}: color.RGBA{127, 127, 127, 255},
-	/* Maroon */ HSL{0, 1.0, 0.25}: color.RGBA{127, 0, 0, 255},
-	/* Olive */ HSL{60, 1.0, 0.25}: color.RGBA{127, 127, 0, 255},
-	/* Green */ HSL{120, 1.0, 0.25}: color.RGBA{0, 127, 0, 255},
-	/* Purple */ HSL{300, 1.0, 0.25}: color.RGBA{127, 0, 127, 255},
-	/* Teal */ HSL{180, 1.0, 0.25}: color.RGBA{0, 127, 127, 255},
-	/* Navy */ HSL{240, 1.0, 0.25}: color.RGBA{0, 0, 127, 255},
+var testCases = map[string]struct {
+	hsl      HSL
+	expected color.RGBA
+}{
+	"Black":   {HSL{0, 0.0, 0.0}, color.RGBA{0, 0, 0, 255}},
+	"White":   {HSL{0, 0.0, 1.0}, color.RGBA{255, 255, 255, 255}},
+	"Red":     {HSL{0, 1.0, 0.5}, color.RGBA{255, 0, 0, 255}},
+	"Lime":    {HSL{120, 1.0, 0.5}, color.RGBA{0, 255, 0, 255}},
+	"Blue":    {HSL{240, 1.0, 0.5}, color.RGBA{0, 0, 255, 255}},
+	"Yellow":  {HSL{60, 1.0, 0.5}, color.RGBA{255, 255, 0, 255}},
+	"Cyan":    {HSL{180, 1.0, 0.5}, color.RGBA{0, 255, 255, 255}},
+	"Magenta": {HSL{300, 1.0, 0.5}, color.RGBA{255, 0, 255, 255}},
+	"Silver":  {HSL{0, 0.0, 0.75}, color.RGBA{191, 191, 191, 255}},
+	"Gray":    {HSL{0, 0.0, 0.5}, color.RGBA{127, 127, 127, 255}},
+	"Maroon":  {HSL{0, 1.0, 0.25}, color.RGBA{127, 0, 0, 255}},
+	"Olive":   {HSL{60, 1.0, 0.25}, color.RGBA{127, 127, 0, 255}},
+	"Green":   {HSL{120, 1.0, 0.25}, color.RGBA{0, 127, 0, 255}},
+	"Purple":  {HSL{300, 1.0, 0.25}, color.RGBA{127, 0, 127, 255}},
+	"Teal":    {HSL{180, 1.0, 0.25}, color.RGBA{0, 127, 127, 255}},
+	"Navy":    {HSL{240, 1.0, 0.25}, color.RGBA{0, 0, 127, 255}},
 }
 
 func TestHSLToColour(t *testing.T) {
-	for input, expected := range testCases {
-		actual := HSLToColour(input)
-		if actual != expected {
-			t.Errorf("input: %v, actual: %v, expected: %v", input, actual, expected)
-		}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			actual := HSLToColour(tc.hsl)
+			if actual != tc.expected {
+				t.Errorf("input: %v, actual: %v, expected: %v", tc.hsl, actual, tc.expected)
+			}
+		})
 	}
 }
