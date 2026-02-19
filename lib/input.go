7e9456879f96ae63c88189b131781019954f6958
Tom <tom@msbit.com.au>
2026-02-19 11:53:01 +1100

    chore: clean up loop

--- lib/input.go
+++ lib/input.go
@@ -17,25 +17,23 @@ func ReadInput(input string) ([]string, map[float64][]float64, error) {
 
 	reader := csv.NewReader(f)
 
-	labels, err := reader.Read()
-	if err != nil {
-		if err == io.EOF {
-			return nil, nil, nil
-		}
-
-		return nil, nil, err
-	}
+	var labels []string
 
 	data := map[float64][]float64{}
 
 	for {
 		row, err := reader.Read()
-		if err != nil {
-			if err == io.EOF {
-				return labels, data, nil
-			}
+		if err == io.EOF {
+			return labels, data, nil
+		}
 
+		if err != nil {
 			return nil, nil, err
+		}
+
+		if labels == nil {
+			labels = row
+			continue
 		}
 
 		x, err := strconv.ParseFloat(row[0], 64)
@@ -43,14 +41,14 @@ func ReadInput(input string) ([]string, map[float64][]float64, error) {
 			return nil, nil, err
 		}
 
-		series := []float64{}
+		ys := []float64{}
 		for _, value := range row[1:] {
 			y, err := strconv.ParseFloat(value, 64)
 			if err != nil {
 				return nil, nil, err
 			}
-			series = append(series, y)
+			ys = append(ys, y)
 		}
-		data[x] = series
+		data[x] = ys
 	}
 }
